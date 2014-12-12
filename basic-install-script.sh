5fe68f0a38618fe96e1185e6c1f859bb1dd5cb19
Buzz <vpalichev@users.noreply.github.com>
2014-12-13 04:56:16 +0500

    Update basic-install-script.sh

--- basic-install-script.sh
+++ basic-install-script.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
 
+
+#Rough plan of web server installation:
+# 1. Enable yum keepcache (ok):
+# Set "keepcache = 1" in "/etc/yum.conf"
+# 
+# 2.
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+
+
+
+# 1. Enable yum caching
+sed -i 's/keepcache=0/keepcache=1/g' /etc/yum.conf
+
+
